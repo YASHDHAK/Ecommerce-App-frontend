009c696d0df7e7d18eaa59cdfbf6dcaf3a9100fa
YASHDHAK <malavyash100@gmail.com>
2025-08-02 18:35:05 +0530

    initial commit

---
+++ src/context/ProductContext.jsx
@@ -0,0 +1,76 @@
+import {useContext, createContext,useState,useEffect} from 'react'
+import axios from 'axios';
+import { server } from "@/main";
+const ProductContext = createContext();
+
+export const ProductProvider = ({children}) => {
+   const [products, setProducts] = useState([]);
+    const [newProd, setNewProd] = useState([]);
+   const [loading, setLoading] = useState(true);
+   const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+    const [search, setSearch] = useState("");
+    const [category, setCategory] = useState("");
+    const [price, setPrice] = useState("");
+    const [categories, setCategories] = useState([]);
+   async function fetchProducts(){
+    setLoading(true);
+    try{
+       const { data } = await axios.get(
+        `${server}/api/product/all?search=${search}&category=${category}&sortByPrice=${price}&page=${page}`
+      );
+
+    setProducts(data.products);
+    setNewProd(data.newProduct);
+    setCategories(data.categories);
+    setTotalPages(data.totalPages);
+    setLoading(false);
+    }catch(error){
+      console.log(error);
+      setLoading(false);
+    }
+   }
+
+  const [product, setProduct] = useState([]);
+  const [relatedProduct, setRelatedProduct] = useState([]);
+
+  async function fetchProduct(id) {
+    setLoading(true);
+    try {
+      const { data } = await axios.get(`${server}/api/product/${id}`);
+
+      setProduct(data.product);
+      setRelatedProduct(data.relatedProduct);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  }
+
+     useEffect(() => {
+    fetchProducts();
+  }, [search,category,page,price]);
+
+    return(
+     <ProductContext.Provider value={{loading,products,newProd,
+        search,
+        setSearch,
+        categories,
+        category,
+        setCategory,
+        totalPages,
+        price,
+        setPrice,
+        page,
+        setPage, 
+        fetchProducts,
+        fetchProduct,
+        product,
+        relatedProduct,
+     }}> 
+      {children}
+    </ProductContext.Provider>
+    );
+};
+export const ProductData = () => useContext(ProductContext);
